20d9a9aa8af2f688571a0b68f5a34c9f607bc8c3
James <jamesrp13@gmail.com>
2016-02-23 13:20:00 -0700

    Test change to demonstrate committing

--- Timeline/ViewController.m
+++ Timeline/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // This is an objective c file but should have swift
 }
 
 - (void)didReceiveMemoryWarning {
