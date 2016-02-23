fa3dc81237f31535850126cb9c4a7076fcc1f37d
James <jamesrp13@gmail.com>
2016-02-23 13:17:58 -0700

    Added tableView and corresponding vc file

---
+++ Timeline/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Timeline
+//
+//  Created by James Pacheco on 2/23/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
